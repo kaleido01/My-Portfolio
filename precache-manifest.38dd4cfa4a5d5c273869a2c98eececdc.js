29c9b3390a8208e93d4b5b0dc022846b4c4e2bb7
kaleido <kaleido@gmail.com>
2019-03-31 15:31:09 +0900

    Updates

---
+++ precache-manifest.38dd4cfa4a5d5c273869a2c98eececdc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d84bef027c36ba01b195",
+    "url": "/My-Portfolio/static/js/runtime~main.e9d7d2f3.js"
+  },
+  {
+    "revision": "256e49438da1e1485cf2",
+    "url": "/My-Portfolio/static/js/main.29e37f7d.chunk.js"
+  },
+  {
+    "revision": "758aeb8e4e21ff585a1a",
+    "url": "/My-Portfolio/static/js/2.710c5493.chunk.js"
+  },
+  {
+    "revision": "2af0dfd77d82d0b4764267ea1157329b",
+    "url": "/My-Portfolio/index.html"
+  }
+];
